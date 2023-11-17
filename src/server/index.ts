ff457a4a3f41d8028d311caca54391be582ecd88
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-17 21:48:06 +0100

    Add ping endpoint

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,8 @@
 import morgan from "morgan";
-import app from "./app";
+import app from "./app.js";
 import express from "express";
+import pingRouter from "../features/ping/router/PingRouter.js";
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use("/", pingRouter);
