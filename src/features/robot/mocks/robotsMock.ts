4ba45ba1adfb9085e5775a4de13f869f7a2165a7
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-18 14:23:22 +0100

    Add test of RobotsController

---
+++ src/features/robot/mocks/robotsMock.ts
@@ -0,0 +1,23 @@
+export const robotsMock = [
+  {
+    _id: "1",
+    name: "Optimus Prime",
+    imageUrl: "http://www.fake.com/url.jpeg",
+    speed: 5,
+    endurance: 8,
+  },
+  {
+    _id: "2",
+    name: "Mazinger Z",
+    imageUrl: "http://www.fake.com/url1.jpeg",
+    speed: 4,
+    endurance: 7,
+  },
+  {
+    _id: "3",
+    name: "Bender",
+    imageUrl: "http://www.fake.com/url2.jpeg",
+    speed: 2,
+    endurance: 9,
+  },
+];
