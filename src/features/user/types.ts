3375acb896270f600d013c60497ff7d65d816233
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-20 22:10:00 +0100

    Add types file to user feature

---
+++ src/features/user/types.ts
@@ -0,0 +1,10 @@
+export interface UserStructure {
+  _id: string;
+  name: string;
+  username: string;
+  password: string;
+}
+
+export type UserDataStructure = Omit<UserStructure, "_id">;
+
+export type UserWithoutPasswordStructure = Omit<UserStructure, "password">;
